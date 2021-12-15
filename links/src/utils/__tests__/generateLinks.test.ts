97aca052b1d1ec89fca3c93b6b09a138ecd85ebd
ccq-sandeep-kaur <94878298+ccq-sandeep-kaur@users.noreply.github.com>
2021-12-15 16:06:10 +1000

    :art: suggestion-resolved #1

--- links/src/utils/__tests__/generateLinks.test.ts
+++ links/src/utils/__tests__/generateLinks.test.ts
@@ -2,36 +2,6 @@ import { generateLinks, totree } from "../generateLinks";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
 
-type types = {
-	id: number;
-	parent_id: number;
-};
-type type2 = {
-	id: number;
-	parent_id: number;
-	children: {
-		id: number;
-		parent_id: number;
-	};
-};
-
-const tree2: types[] = [
-	{
-		id: 76174760,
-		parent_id: 75884025,
-	},
-];
-const tree3: type2[] = [
-	{
-		id: 90833231,
-		parent_id: 75884024,
-		children: {
-			id: 90833232,
-			parent_id: 90833231,
-		},
-	},
-];
-
 test("generateLinks: ", () => {
 	generateLinks().then((data) => {
 		assert.equal(data, []);
@@ -48,6 +18,34 @@ test("totree: array with one child", () => {
 	assert.equal(tree, tree2);
 });
 
+type types = {
+	id: number;
+	parent_id: number;
+};
+type type2 = {
+	id: number;
+	parent_id: number;
+	children: {
+		id: number;
+		parent_id: number;
+	};
+};
+const tree2: types[] = [
+	{
+		id: 76174760,
+		parent_id: 75884025,
+	},
+];
+const tree3: type2[] = [
+	{
+		id: 90833231,
+		parent_id: 75884024,
+		children: {
+			id: 90833232,
+			parent_id: 90833231,
+		},
+	},
+];
 test("totree: array with grand child", () => {
 	let tree = totree(tree3, 75884024);
 	assert.equal(tree, tree3);
