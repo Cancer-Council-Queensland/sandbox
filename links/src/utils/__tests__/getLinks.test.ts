7bd0c5d7029a4af1d6166ca027e57aab9a302401
ccq-sandeep-kaur <94878298+ccq-sandeep-kaur@users.noreply.github.com>
2021-12-15 15:52:41 +1000

    Update links/src/utils/__tests__/getLinks.test.ts
    Co-authored-by: Jamie Martin <94496429+ccq-jamie-martin@users.noreply.github.com>

--- links/src/utils/__tests__/getLinks.test.ts
+++ links/src/utils/__tests__/getLinks.test.ts
@@ -2,7 +2,6 @@ import { test } from "uvu";
 import * as assert from "uvu/assert";
 import { getLinks } from "../getLinks";
 
-//test case
 test("getLinks:", () => {
 	getLinks().then((data) => {
 		assert.instance(data, Object);
