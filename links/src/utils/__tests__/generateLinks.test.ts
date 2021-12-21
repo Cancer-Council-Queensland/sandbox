b2d4d24177662ff7a814d94ae38f830f3cbf99dd
ccq-sandeep-kaur <94878298+ccq-sandeep-kaur@users.noreply.github.com>
2021-12-21 14:35:24 +1000

    :white_check_mark:  feat-parents-siblings-tree
    Fun()  to get the parent of given slug
    Fun()  to get the siblings of given slug

--- links/src/utils/__tests__/generateLinks.test.ts
+++ links/src/utils/__tests__/generateLinks.test.ts
@@ -1,49 +1,42 @@
-import { generateLinks, totree } from "../generateLinks";
+import { generateLinks, toTree, Tree } from "../generateLinks";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
 
-test("generateLinks: ", () => {
-	generateLinks().then((data) => {
-		assert.equal(data, []);
-	});
-});
-
-test("totree: empty array", () => {
-	let tree = totree([], 0);
-	assert.equal(tree, []);
-});
-
-test("totree: array with one child", () => {
-	let tree = totree(tree2, 75884025);
-	assert.equal(tree, tree2);
-});
-
-type type2 = {
-	id: number;
-	parent_id: number;
-	children: {
-		id: number;
-		parent_id: number;
-	};
-};
-const tree2: types[] = [
+const tree2: Tree = [
 	{
 		id: 76174760,
 		parent_id: 75884025,
+		slug: "onoty/",
 	},
 ];
-const tree3: type2[] = [
+
+const tree3: Tree = [
 	{
 		id: 90833231,
 		parent_id: 75884024,
-		children: {
-			id: 90833232,
-			parent_id: 90833231,
-		},
+		slug: "cancerqld/research",
+		children: [
+			{
+				id: 90833232,
+				parent_id: 90833231,
+				slug: "cancerqld/research/viertel-cancer-research-centre",
+			},
+		],
 	},
 ];
+
+test("generateLinks: ", async () => {
+	let data = await generateLinks();
+	assert.instance(data, Array);
+});
+
+test("totree: array with one child", () => {
+	let tree = toTree(tree2, 75884025);
+	assert.equal(tree, tree2);
+});
+
 test("totree: array with grand child", () => {
-	let tree = totree(tree3, 75884024);
+	let tree = toTree(tree3, 75884024);
 	assert.equal(tree, tree3);
 });
 
--- links/src/utils/generateLinks.ts
+++ links/src/utils/generateLinks.ts
@@ -1,4 +1,3 @@
-import fs from "fs";
 import { getLinks } from "./getLinks";
 
 type GenerateLinks = {
@@ -12,19 +11,74 @@ type GenerateLinks = {
 export const generateLinks: GenerateLinks = async () => {
 	const links = await getLinks();
 	const arr = Object.values(links);
-	let tree = totree(arr, 0);
-
-	return tree;
+	getSiblings(arr, "cancerqld/research");
+	getParents(arr, "cancerqld/research/viertel-cancer-research-centre");
+	return;
 };
 
-export const totree = (arr: any[], parent_id: number = 0) => {
-	let tree = [];
+export type Node = {
+	id: number;
+	parent_id: number;
+	slug: string;
+};
+
+export type NodeWithChildren = Node & { children?: NodeWithChildren[] };
+
+export type Tree = NodeWithChildren[];
+
+export type ToTree = {
+	(arr: Node[], parent_id?: number): Tree;
+};
+
+export type GetSiblings = {
+	(arr: Node[], slug: string): Tree;
+};
+
+// getSiblings(tree, '/cancerqld/research')
+export const getSiblings: GetSiblings = (arr, slug) => {
+	let tree: Tree = [];
+	let parent: number;
+	for (const node of arr) {
+		if (node.slug === slug) {
+			parent = node.parent_id;
+			tree = toTree(arr, parent);
+		}
+	}
+	return tree;
+};
+export const toTree: ToTree = (arr, parent_id = 0) => {
+	let tree: Tree = [];
 	for (const node of arr) {
 		// use key value to get parent
 		if (node.parent_id === parent_id) {
-			let children = totree(arr, node.id);
-			if (children.length) {
-				node.children = children;
+			let child_tree = toTree(arr, node.id);
+			let children = child_tree.length ? child_tree : undefined;
+			tree.push({ ...node, children });
+		}
+	}
+	return tree;
+};
+
+// getParents(tree, '/cancerqld/research') get parent_id of slug
+export const getParents: GetSiblings = (arr, slug) => {
+	let tree: Tree = [];
+	for (const node of arr) {
+		if (node.slug === slug) {
+			tree = closestParent(arr, node.parent_id);
+			tree.push(node);
+		}
+	}
+	return tree;
+};
+
+//closesrparent - get the second closest parent_id
+export const closestParent: ToTree = (arr, parent) => {
+	let tree: Tree = [];
+	for (const node of arr) {
+		if (node.id === parent) {
+			if (node.parent_id !== 0) {
+				//recursion -  to get the gradparents
+				tree = closestParent(arr, node.parent_id);
 			}
 			tree.push(node);
 		}
